74e563e21bd646f0335077f2924b65cc0ef1da3e
Oreomial <106767901+Oreomial@users.noreply.github.com>
2026-01-18 16:05:51 +0100

    -
    Removed commented-out code related to event dimension.

--- scripts/modules/generators.js
+++ scripts/modules/generators.js
@@ -3,7 +3,6 @@ import { system } from "@minecraft/server";
 const CustomComponent = {
     onTick(event) {
     console.warn("Custom component tick event triggered", event.block.typeId);
-    //event.dimension // Dimension that contains the block.
     }
 }
 
